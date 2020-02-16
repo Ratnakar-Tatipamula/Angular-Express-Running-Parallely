bb4b604ebddff341907820af0a6657a6a84b1ba1
Ratnakar-Tatipamula <47071239+Ratnakar-Tatipamula@users.noreply.github.com>
2020-02-16 15:46:59 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+
+const port = 3000;
+var express = require('express');
+var app = express();
+const path = require('path');
+const bodyparser = require('body-parser');
+
+ const api = require('./server/routes/api');
+
+ app.use('/api',api);
+
+//app.use(express.static(path.join(__dirname,'dist/my-app')))
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+
+app.get('*',(req,res)=>{
+         //res.sendFile(path.join(__dirname,'dist/my-app/index.html'));
+        // res.sendFile(path.join(__dirname, 'dist/my-app/index.html'));
+        // res.sendFile(path.join(__dirname, './src', 'index.html'));
+   });
+    
+
+app.listen(port,function(){
+    console.log("Server running on localhost:" + port);
+});
+
